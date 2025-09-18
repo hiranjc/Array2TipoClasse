5b76ac69edec3edc486eec18c57765b63f8d328e
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:05:35 -0300

    first commit

---
+++ Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Array2TipoClasse
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Array2TipoClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            Product[] vect = new Product[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += vect[i].Price;
+            }
+            double avg = sum / N;
+            Console.WriteLine("AVERAGE PRICE: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
